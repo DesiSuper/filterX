983725371f9c9bcf3a20086e375b1772fb33787b
DesiSuper <102736089+DesiSuper@users.noreply.github.com>
2024-01-25 18:16:49 +0530

    Update openai.py

--- plugins/openai.py
+++ plugins/openai.py
@@ -5,19 +5,14 @@ from openai import OpenAI
 
 ai_client = OpenAI(api_key=OPENAI_API)
 
-@Client.on_message(filters.command("openai"))
+@Client.on_message(filters.command("ask"))
 async def ask_question(client, message):
     if len(OPENAI_API) == 0:
         return await message.reply("OPENAI_API is empty")
-    if message.chat.id != SUPPORT_GROUP:
-        btn = [[
-            InlineKeyboardButton('Support Group', url=SUPPORT_LINK)
-        ]]
-        return await message.reply("This command only working in support group.", reply_markup=InlineKeyboardMarkup(btn))
     try:
         text = message.text.split(" ", 1)[1]
     except:
-        return await message.reply_text("Command Incomplete!\nUsage: /openai your_question")
+        return await message.reply_text("Command Incomplete!\nUsage: /ask your_question")
     msg = await message.reply("Searching...")
     try:
         response = ai_client.chat.completions.create(
@@ -26,6 +21,6 @@ async def ask_question(client, message):
             ],
             model="gpt-3.5-turbo"
         )
-        await msg.edit(f"User: {message.from_user.mention}\nQuery: <code>{text}</code>\n\nResults:\n\n<code>{response.choices[0].message.content}</code>")
+        await msg.edit(f"💬 Query: <code>{text}</code>\n🦹 User: {message.from_user.mention}\n⚡ Results:\n<code>{response.choices[0].message.content}</code>")
     except Exception as e:
         await msg.edit(f'Error - <code>{e}</code>')
